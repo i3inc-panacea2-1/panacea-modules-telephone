9cf225e40aea3a5fdf9d9074d0535e4eba97783a
Portno <j.remoundos@dotbydot.gr>
2019-06-24 13:16:00 +0300

    nuget + text size

---
+++ src/Panacea.Modules.Telephone/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace Panacea.Modules.Telephone
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        public App()
+        {
+
+        }
+    }
+}
+
